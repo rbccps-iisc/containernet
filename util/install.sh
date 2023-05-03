1c707c747e43c22b2dffb1b5f26c06779f0f67e2
Ramon Fontes <ramonreisfontes@gmail.com>
2023-05-03 10:34:20 -0300

    install.sh: add update

--- util/install.sh
+++ util/install.sh
@@ -214,6 +214,7 @@ function mn_wifi_deps {
     echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
          "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
          sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    $update update
     $install docker-ce
     sudo PYTHON=${PYTHON} pip install docker
 
